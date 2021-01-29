11d29019606e8a61ca2172df4702c4ea9e8f1451
lindsey <lindseyraven6@gmail.com>
2021-01-29 11:11:45 +0000

    app.py & trello_api.py changes
    changed session.?? with trello.?? to route to trello_api.py.  simplified the started, completed and update functions.

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,6 @@
 from flask import render_template, Flask, redirect, request
 from todo_app.data import session_items as session
+from todo_app.data import trello_api as trello
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
@@ -7,48 +8,42 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    all_items = session.get_items()
+    all_items = trello.get_items()
     return render_template('index.html', items = all_items)
 
 @app.route('/add-todo', methods=['POST'])
 def add_todo():
-    item = request.form.get('name')
-    session.add_item(item)
+    title = request.form.get('name')
+    trello.add_item(title)
     return redirect('/')
 
 #delete button
 @app.route('/delete-todo', methods=['POST'])
 def delete_todo():
     todo_id = request.form.get('todo_id')
-    session.delete_item(todo_id)
+    trello.delete_item(todo_id)
     return redirect('/')
 
 #completed status
 @app.route('/complete-todo', methods=['POST'])
 def complete_todo():
     todo_id = request.form.get('todo_id')
-    item = session.get_item(todo_id)
-    item['status'] = 'completed'
-    session.save_item(item)
+    trello.complete_item(todo_id)
     return redirect('/')
 
 #started status
 @app.route('/started-todo', methods=['POST'])
 def started_todo():
     todo_id = request.form.get('todo_id')
-    item = session.get_item(todo_id)
-    item['status'] = 'in progress'
-    session.save_item(item)
+    trello.started_item(todo_id)
     return redirect("/")
 
 #update title
 @app.route('/update-title', methods=['POST'])
 def update_title():
     todo_id = request.form.get('todo_id')
-    item = session.get_item(todo_id)
     new_todo_title = request.form.get('title')
-    item['title'] = new_todo_title
-    session.save_item(item)
+    trello.update_title(new_todo_title, todo_id)
     return redirect('/')
 
 if __name__ == '__main__':
--- todo_app/data/trello_api.py
+++ todo_app/data/trello_api.py
@@ -1,40 +1,17 @@
-import os
+def get_items():
+    pass 
 
-@app.route('/')
-def index():
-    todos=[]
+def add_item(title):
+    pass 
 
-#Fetch all to-do items (cards) for the specified board
-    cards_url = f"https://api.trello.com/1/boards/{os.getenv('Trello_Board_ID')}/cards"
+def delete_item(todo_id):
+    pass
 
-    query = {
-        'key': os.getenv('Trello_API_Key'),
-        'token': os.getenv('Trello_API_Token')
-        }
+def complete_item(todo_id):
+    pass
 
-    response = requests.get(
-        cards_url,
-        params=query
-        )
-    trello_data=response.json()
-    for trello_card in trello_data: 
-        todos.append({"id": trello_card["id"], "status": get_status(trello_card["idList"]), "title": trello_card["name"]})
-    print(trello_data)
-    return render_template('index.html', items = todos)
+def started_item(todo_id):
+    pass
 
-
-def get_status(id):
-    query = {
-        'key': os.getenv('Trello_API_Key'),
-        'token': os.getenv('Trello_API_Token')
-        }
-        
-    status_url = f"https://api.trello.com/1/boards/{os.getenv('Trello_Board_ID')}/lists" #needs to be lists at the end see trello docs
-    response=requests.get(
-        status_url,
-        params=query)
-    
-    statuses=response.json()
-    for card_status in statuses:
-        if card_status["id"]==id:
-            return card_status["name"]
+def update_title(new_todo_title, todo_id):
+    pass
