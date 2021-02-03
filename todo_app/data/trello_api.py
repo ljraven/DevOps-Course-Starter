a35d1440400ed6948b8d91de96ca381e36bee124
lindsey <lindseyraven6@gmail.com>
2021-02-03 18:19:35 +0000

    get_items module 2
    linking my vs code to trello. on trello api document go to  go to 'get cards for boards' and under 'example' select python. use this as a basic script to insert into your 'trello_api.py' on vs code. replace the {id) with your board id and then insert your key and token. ensure import requests is placed at the top above everything. if it flags an error then make sure you add it into your toml file. in terminal type 'poetry add requests' this should then automatically add it. back on your trello_api.py file. 
    ensure that this script is told to return a response. such as 'return response.json()

--- todo_app/data/trello_api.py
+++ todo_app/data/trello_api.py
@@ -1,5 +1,22 @@
+import requests 
+
 def get_items():
-    pass 
+    # This code sample uses the 'requests' library:
+    # http://docs.python-requests.org
+    url = "https://api.trello.com/1/boards/60103b91b643743d059ee1bb/cards"
+
+    query = {
+    'key': 'fb08e8a1c59ad3a564b8db2418f9c697',
+    'token': 'a842bc9d83091396464aa788df89e400015931af2f13bb2aaae9e2c5b673f1e2'
+    }
+
+    response = requests.get(
+    url,
+    params=query
+    )
+
+    return response.json()
+
 
 def add_item(title):
     pass 
